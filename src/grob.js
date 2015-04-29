31c545f4ef62e7fb34d18238a31fc6512bc9b88c
stebanos <stefan.gabriels@gmail.com>
2015-04-29 16:08:08 +0200

    Added the random generator.

--- src/grob.js
+++ src/grob.js
@@ -6,6 +6,29 @@ var img = require('img.js');
 
 var grob = {};
 
+grob.randomGenerator = function (seed) {
+    // Note: the generator didn't work with negative seed values, so here we
+    // transform our original seed into a new (positive) seed value with which we
+    // create a new generator.
+    if (seed < 0) {
+        var gen = grob.randomGenerator(Math.abs(seed));
+        for (var i = 0; i < 23; i += 1) {
+            gen();
+        }
+        return grob.randomGenerator(gen(0, 10000));
+    }
+
+    // Based on random number generator from
+    // http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+    return function (min, max) {
+        min = min || 0;
+        max = max || 1;
+        seed = (seed * 9301 + 49297) % 233280;
+        var v = seed / 233280;
+        return min + v * (max - min);
+    };
+};
+
 // Commands
 function importCommands(module) {
     for (var k in module) {
