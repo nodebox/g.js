623b1a6337cdc135f5ca4128545c72a40454d456
stebanos <stefan.gabriels@gmail.com>
2015-04-29 13:49:57 +0200

    Import functions.

--- src/grob.js
+++ src/grob.js
@@ -23,6 +23,74 @@ for (var k in img) {
     grob[k] = img[k];
 }
 
+grob.importSVG = function (svgString) {
+    return g.svg.parseString(svgString);
+};
+
+grob.importImage = function (image) {
+    var layer = g.Layer.fromImage(image);
+    return new g.Img(layer.toCanvas());
+};
+
+grob.importCSV = function (csvString, delimiter) {
+    // Try to parse the string as a number.
+    // Return a number or else the string, unchanged.
+    function parseNumber(s) {
+        var v = parseFloat(s);
+        return isNaN(v) ? s : v;
+    }
+
+    // Split the row, taking quotes into account.
+    function splitRow(s, delimiter) {
+        var row = [], c, col = '', i, inString = false;
+        s = s.trim();
+        for (i = 0; i < s.length; i += 1) {
+            c = s[i];
+            if (c === '"') {
+                if (s[i+1] === '"') {
+                    col += '"';
+                    i += 1;
+                } else {
+                    inString = !inString;
+                }
+            } else if (c === ',') {
+                if (!inString) {
+                    row.push(col);
+                    col = '';
+                } else {
+                    col += c;
+                }
+            } else {
+                col += c;
+            }
+        }
+        row.push(col);
+        return row;
+    }
+
+    var rows, header;
+    delimiter = delimiter || ',';
+
+    if (!csvString) return null;
+    rows = csvString.split(/\r\n|\r|\n/g);
+    header = splitRow(rows[0], delimiter);
+    rows = rows.slice(1);
+
+    rows = _.reject(rows, _.isEmpty);
+
+    rows = _.map(rows, function (row) {
+        var cols, m = {};
+        cols = _.map(splitRow(row, delimiter), function (col) {
+            return parseNumber(col);
+        });
+        _.each(cols, function (col, index) {
+            m[header[index]] = col;
+        });
+        return m;
+    });
+    return rows;
+};
+
 grob.merge = function () {
     var objects = _.flatten(arguments, true);
     if (Array.isArray(objects) && objects.length > 0) {
