4ef0c579a607a38337fbf9f36f176b35eb7092d2
stebanos <stefan.gabriels@gmail.com>
2019-04-11 10:02:12 +0300

    Version 0.1.17

--- dist/g.js
+++ dist/g.js
@@ -22627,6 +22627,7 @@ function constructPath(points, closed) {
 }
 
 vg.roundedSegments = function (shape, d) {
+    if (!d || d.length === 0) { return shape; }
     const points = vg.toPoints(shape);
     const newPoints = [];
     for (let i = 0; i < points.length; i +=1) {
